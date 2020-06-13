45adf7446af4ae3767b58af1a5e16b9275c1d3ca
l0rzl <o.gat.ak.u5+githorz@gmail.com>
2020-06-13 09:35:47 +0000

    Update Gitpod settings and memos

--- gitpod/setup.rbenv.sh
+++ gitpod/setup.rbenv.sh
@@ -4,4 +4,6 @@ brew install rbenv ruby-build
 rbenv install 2.7.1
 rbenv init 2>> ~/.bash_profile
 #rbenv rehash
-echo Type 'eval "$(rbenv init -)"' or 'source ~/.bash_profile'
+rbenv exec gem install bundler
+echo Set shell environment: 'eval "$(rbenv init -)"' or 'source ~/.bash_profile'
+echo Make a new Gemfile: 'rbenv exec bundle init'
